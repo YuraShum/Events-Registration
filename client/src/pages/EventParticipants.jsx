4abefc6e7089f74fa6509a9decf24e8f4020a363
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-09-19 19:17:54 +0300

    added the ability to search for users

--- client/src/pages/EventParticipants.jsx
+++ client/src/pages/EventParticipants.jsx
@@ -5,6 +5,7 @@ import ReturnLink from '../components/ui/ReturnLink'
 import ListenerItem from '../components/ui/ListenerItem'
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
 
 const EventParticipants = () => {
 
@@ -13,13 +14,15 @@ const EventParticipants = () => {
   const [allParticipants, setAllParticipants] = useState([])
   const [currentPage, setCurrentPage] = useState(1);
   const [eventTitle, setEventTitle] = useState("")
+  const [searchValue, ssetSearchValue] = useState("")
+  const [filteredParticipants, setFilteredParticipants] = useState([])
 
   const step = 6
-  const totalPages = Math.ceil(allParticipants.length / step);
+  const totalPages = Math.ceil(filteredParticipants.length / step);
 
   const indexOfLastParticipants = currentPage * step;
   const indexOfFirstParticipants = indexOfLastParticipants - step;
-  const currentParticipants = allParticipants.slice(indexOfFirstParticipants, indexOfLastParticipants);
+  const currentParticipants = filteredParticipants.slice(indexOfFirstParticipants, indexOfLastParticipants);
 
 
   useEffect(() => {
@@ -29,6 +32,7 @@ const EventParticipants = () => {
       if (response) {
         setAllParticipants(response.userListeners)
         setEventTitle(response.title)
+        setFilteredParticipants(response.userListeners)
       }
       if (error) {
         console.log(error)
@@ -52,12 +56,36 @@ const EventParticipants = () => {
     }
   }
 
+  const handleSearch = (event) => {
+    const eventValue = event.target.value
+
+    ssetSearchValue(eventValue)
+
+    const filtered = allParticipants.filter(participant =>
+      participant.email.toLowerCase().includes(eventValue.toLowerCase()) ||
+      participant.fullname.toLowerCase().includes(eventValue.toLowerCase())
+    )
+    setFilteredParticipants(filtered)
+    setCurrentPage(1)
+  }
+
 
   return (
     <div className='text-white'>
       <h2 className='text-4xl font-bold text-custom text-center pt-4'>Event Participants</h2>
       <p className='italic opacity-80 text-lg text-center'>({eventTitle})</p>
-      <ReturnLink />
+      <div className='flex justify-between items-center px-6'>
+        <ReturnLink />
+        <div className='relative text-black '>
+          <CiSearch className='absolute top-[12px] left-0 w-6 h-6' />
+          <input
+            className='bg-gray-100 p-2 text-sm md:text-lg border-gray-300 border-2 rounded-lg pl-6'
+            type="text"
+            placeholder='Search'
+            value={searchValue}
+            onChange={handleSearch} />
+        </div>
+      </div>
       <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-8 p-4 ">
         {currentParticipants.map(listener => (
           <ListenerItem key={listener._id} listener={listener} />
