4f7af1341b0b285bd5e59e0104d258fad8cd7747
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-09-21 11:09:50 +0300

    fix: adaptive positioning of the icon in the search

--- client/src/pages/EventParticipants.jsx
+++ client/src/pages/EventParticipants.jsx
@@ -84,7 +84,7 @@ const EventParticipants = () => {
           <div className='flex justify-between items-center px-6 mt-4'>
             <ReturnLink />
             <div className='relative text-black'>
-              <CiSearch className='absolute top-[12px] left-0 w-6 h-6' />
+              <CiSearch className='absolute top-[8px] md:top-[12px] left-0 w-6 h-6' />
               <input
                 className='bg-gray-100 p-2 text-sm md:text-lg border-gray-300 border-2 rounded-lg pl-6'
                 type="text"
